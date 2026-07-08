8546bac0e299d7c25ffb23a7b9b3b353438c04f9
Toan Huynh <toanhuynh.apr@gmail.com>
2026-07-08 11:03:41 +0700

    feat: add global exception filter and response interceptor; update dependencies

---
+++ src/common/interceptors/reponse.interceptor.ts
@@ -0,0 +1,20 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { map, Observable } from "rxjs";
+
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Record<string, unknown>> {
+    intercept(
+        context: ExecutionContext, 
+        next: CallHandler<T>
+    ): Observable<Record<string, unknown>> {
+        return next.handle().pipe(
+            map((data) => ({
+                success: true,
+                message: "Request successful",
+                data,
+                timestamp: new Date().toISOString(),
+            })),
+        );
+    }
+}
