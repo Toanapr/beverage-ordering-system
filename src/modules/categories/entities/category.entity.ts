bf2a324f88782beead2ea7e4d608bb04d764b797
Toan Huynh <toanhuynh.apr@gmail.com>
2026-07-08 15:09:36 +0700

    feat: add Categories module and Category entity for product categorization

---
+++ src/modules/categories/entities/category.entity.ts
@@ -0,0 +1,31 @@
+import { Product } from "src/modules/products/entities/product.entity";
+import { Store } from "src/modules/stores/entities/store.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("categories")
+export class Category {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ type: "uuid", name: "store_id" })
+    storeId!: string;
+
+    @ManyToOne(() => Store, (store) => store.categories, {
+        onDelete: "CASCADE"
+    })
+    @JoinColumn({ name: "store_id" })
+    store!: Store;
+
+    @OneToMany(() => Product, (product) => product.category)
+    products!: Product[];
+
+    @Column({ type: "varchar", length: 100 })
+    name!: string;
+
+    @CreateDateColumn({ type: "timestamp with time zone", name: "created_at" })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: "timestamp with time zone", name: "updated_at" })
+    updatedAt!: Date;
+
+}
